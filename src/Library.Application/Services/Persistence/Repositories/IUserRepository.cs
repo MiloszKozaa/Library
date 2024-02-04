b07fbc1aa66ee70d6b74a1a5415ccbe1c6fcbc9a
m.koza <101021211+MiloszKozaa@users.noreply.github.com>
2024-02-04 21:39:33 +0100

    Remaked user table

--- src/Library.Application/Services/Persistence/Repositories/IUserRepository.cs
+++ src/Library.Application/Services/Persistence/Repositories/IUserRepository.cs
@@ -5,5 +5,4 @@ public interface IUserRepository : IRepository<User>
 {
     Task<bool> UsernameExistsAsync(string username, CancellationToken cancellationToken = default);
     Task<bool> EmailExistsAsync(string email, CancellationToken cancellationToken = default);
-    Task<User> GetUserWithAllDependenciesAsync(Guid id, CancellationToken cancellationToken = default);
 }
--- src/Library.Domain/Models/User.cs
+++ src/Library.Domain/Models/User.cs
@@ -2,15 +2,14 @@
 {
     public class User : Model
     {
-        public Guid AddressId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
         public DateTime BirthDate { get; set; }
-        public Address Address { get; set; }
         public UserRole Role { get; set; }
+        public Address Address { get; set; }
     }
 
     public enum UserRole
